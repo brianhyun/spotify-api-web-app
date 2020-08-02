ab527866310f10d0a783cb068387a8443a467511
Brian Hyun <jso.venn@gmail.com>
2020-08-02 13:18:19 -0700

    Remove config vars

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const https = require('https');
 const express = require('express');
 
 const state = require('../utils/state');
+// const { auth_endpoint, client_id, redirect_uri } = require('../utils/config');
 
 const router = express.Router();
 
@@ -13,16 +14,15 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-	// auth endpoint: https://accounts.spotify.com/authorize
-	// parameters: client_id, response_type, redirect_uri, state 
 	const authEndpoint = `${process.env.auth_endpoint}?client_id=${process.env.client_id}&response_type=code&redirect_uri=${process.env.redirect_uri}&state=${state}`;
 	https.get(authEndpoint, (response) => {
 		response.on('data', (d) => {
-		  process.stdout.write(d);
+			console.log(d);
+			process.stdout.write(d);
 		});
 	}).on('error', (e) => {
 		console.error(e);
 	});
 });
 
-module.exports = router; 
+module.exports = router;
