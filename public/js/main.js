2a03e1b3fb43a30d997426902e774059259f809e
Brian Hyun <jso.venn@gmail.com>
2020-08-07 13:24:24 -0700

    Change dashboard layout

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
+// Hide navbar on login page. 
 if (window.location.pathname === "/") {
 	const navbar = document.querySelector("nav");
 	navbar.style.display = "none";
 }
+
+// Main Dashboard - Toggle Artists and Tracks Display
