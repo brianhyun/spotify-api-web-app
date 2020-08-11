f26cdf67eebc2f6d1386cf7a02db9c50d5a65b3f
Brian Hyun <jso.venn@gmail.com>
2020-08-11 10:58:05 -0700

    Finish functionality for hamburger menu and close button

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ if (window.location.pathname === "/") {
 }
 
 // Hamburger Menu Function
+const mainNav = document.getElementsByClassName("main-navbar");
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const navigationMenu = document.getElementById("navigation-menu");
 const mainSection = document.getElementsByTagName("main");
@@ -13,12 +14,32 @@ hamburgerMenu.addEventListener("click", () => {
 	// Hide Hamburger Menu
 	hamburgerMenu.style.display = "none";
 
+	// Remove Padding on Menu
+	mainNav[0].style.padding = "0";
+
 	// Hide Main Section
 	mainSection[0].style.display = "none";
 
 	// Show Nav Menu 
 	navigationMenu.style.display = "flex";
 });	
+
+// Close Button 
+const closeBtn = document.getElementById("close-btn");
+
+closeBtn.addEventListener("click", () => {
+	// Show Hamburger Menu 
+	hamburgerMenu.style.display = "flex";
+
+	// Add Padding on Menu 
+	mainNav[0].style.padding = "4.5rem 3.5rem";
+
+	// Hide Main Section
+	mainSection[0].style.display = "flex";
+
+	// Hide Nav Menu 
+	navigationMenu.style.display = "none";
+});
 
 // Main Dashboard - Toggle Artists and Tracks Display
 const headers = document.getElementsByClassName("main-dash__top-stats-menu-header");
