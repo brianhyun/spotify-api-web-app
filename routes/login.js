4f3b54e822b6d50d986af3a346cc041a8aafd9fb
Brian Hyun <jso.venn@gmail.com>
2020-08-16 01:35:11 -0700

    Finish recently played page

--- routes/login.js
+++ routes/login.js
@@ -11,13 +11,17 @@ router.get('/', (req, res, next) => {
 	});
 });
 
-// Scopes 
+// Scopes in Use
 // playlist-read-private: Get a List of Current User's Playlists
 // user-top-read: Get a User's Top Artists and Tracks
 // user-follow-read: Get User's Followed Artists
+// user-read-recently-played: Get Current User's Recently Played Tracks
+
+// Tentative Scopes
+// user-read-playback-state, user-read-currently-playing user-library-read user-read-playback-position
 
 router.post('/login', (req, res, next) => {
-	const scopes = 'playlist-read-private user-top-read user-follow-read user-read-playback-state user-read-currently-playing user-library-read user-read-playback-position user-read-recently-played';
+	const scopes = 'playlist-read-private user-top-read user-follow-read user-read-recently-played';
 	res.redirect(`${auth_endpoint}?response_type=code&client_id=${client_id}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirect_uri)}`);
 });
 
--- routes/recent.js
+++ routes/recent.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 
+const library = require('../utils/library');
+
 const router = express.Router();
 
 router.get('/recent', (req, res, next) => {
@@ -10,16 +12,30 @@ router.get('/recent', (req, res, next) => {
 		headers: {'Authorization': `Bearer ${access_token}`}
 	};
 
-	const playlistURL = 'https://api.spotify.com/v1/me/playlists';
+	const recentURL = 'https://api.spotify.com/v1/me/player/recently-played';
 
-	axios.get(playlistURL, options)
+	axios.get(recentURL, options)
 		.then(function (response) {
 
-			console.log(response.data);
+			// An array of objects with a nested object that stores information about the recently played track. 
+			const recentArray = response.data.items;
+
+			// Create an array that holds all the recently played tracks.
+			const tracksArray = [];
+
+			for (let i = 0; i < recentArray.length; i++) {
+				tracksArray.push(recentArray[i].track);
+			}
+
+			// Push tracks array into library function. 
+			// Returns an array of arrays, with each nested array holding info about the artists on each track. 
+			const trackArtists = library.returnArtistsInfoFrom(tracksArray);
 
 			res.render('recent', {
 				path: 'recent', 
-				pageTitle: 'Recently Played'
+				pageTitle: 'Recently Played',
+				recentArray: recentArray,
+				trackArtists: trackArtists
 			});
 		})
 		.catch(function (error) {
