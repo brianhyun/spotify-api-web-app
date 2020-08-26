dfd963e44e2faf4f84328adbe33775ea7616833c
Brian Hyun <jso.venn@gmail.com>
2020-08-26 07:15:21 -0700

    Fix formatting issue with track artist header in track analysis page and rewrite labels for track analysis chart

--- public/js/main.js
+++ public/js/main.js
@@ -13,24 +13,28 @@ if (regExp.test(window.location.pathname)) {
 	var myChart = new Chart(ctx, {
 		type: 'bar',
 		data: {
-			labels: ['Danceability', 'Energy', 'Speechiness', 'Loudness (Reduced by Factor of 10)', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence'],
+			labels: ['Danceability', 'Energy', 'Speechiness', 'Loudness (x10^-1)', 'Acousticness', 'Instrumentalness', 'Liveness', 'Valence'],
 			datasets: [{
 				data: [data.danceability, data.energy, data.speechiness, Math.abs(data.loudness) / 10, data.acousticness, data.instrumentalness, data.liveness, data.valence],
 				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
+					'rgba(46, 204, 113, 0.2)',
 					'rgba(54, 162, 235, 0.2)',
 					'rgba(255, 206, 86, 0.2)',
 					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
+					'rgba(232, 67, 147, 0.2)',
+					'rgba(255, 159, 64, 0.2)',
+					'rgba(230, 126, 34, 0.2)',
+					'rgba(192, 57, 43, 0.2)'
 				],
 				borderColor: [
-					'rgba(255, 99, 132, 1)',
+					'rgba(46, 204, 113, 1)',
 					'rgba(54, 162, 235, 1)',
 					'rgba(255, 206, 86, 1)',
 					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
+					'rgba(232, 67, 147, 1)',
+					'rgba(255, 159, 64, 1)',
+					'rgba(230, 126, 34, 1)',
+					'rgba(192, 57, 43, 1)'
 				],
 				borderWidth: 1
 			}]
